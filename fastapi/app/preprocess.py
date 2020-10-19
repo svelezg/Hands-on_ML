2dd66ea970f4c52b747b9acd1a6f5ca05a655803
svelezg <svelez.velezgarcia@gmail.com>
2020-10-19 10:06:04 -0500

    Fixed preprocessing

--- fastapi/app/preprocess.py
+++ fastapi/app/preprocess.py
@@ -22,26 +22,24 @@ class CustomImputer(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X, y=None):
-        # fill Fare
-        X['Fare'] = X.groupby(['Pclass', 'Sex'])['Fare'] \
-            .transform(lambda x: x.fillna(x.mean()))
-
         # fill Age
-        X['Age'] = X.groupby(['Pclass', 'Sex'])['Age'] \
-            .transform(lambda x: x.fillna(x.mean()))
+
+        X.loc[((np.isnan(X["Age"])) & (X.Pclass == 1) & (X.Sex == 'female')), 'Age'] = 35.197183
+        X.loc[((np.isnan(X["Age"])) & (X.Pclass == 1) & (X.Sex == 'male')), "Age"] = 41.364270
+        X.loc[((np.isnan(X["Age"])) & (X.Pclass == 2) & (X.Sex == 'female')), "Age"] = 28.689394
+        X.loc[((np.isnan(X["Age"])) & (X.Pclass == 2) & (X.Sex == 'male')), "Age"] = 30.91663
+        X.loc[((np.isnan(X["Age"])) & (X.Pclass == 3) & (X.Sex == 'female')), "Age"] = 21.478261
+        X.loc[((np.isnan(X["Age"])) & (X.Pclass == 3) & (X.Sex == 'male')), "Age"] = 26.709258
+
+        # fill Fare
+        X.loc[((np.isnan(X["Fare"])) & (X.Pclass == '1') & (X.Sex == 'female')), "Fare"] = 107.52020875000001
+        X.loc[((np.isnan(X["Fare"])) & (X.Pclass == '1') & (X.Sex == 'male')), "Fare"] = 69.94506203703703
+        X.loc[((np.isnan(X["Fare"])) & (X.Pclass == '2') & (X.Sex == 'female')), "Fare"] = 22.20925294117647
+        X.loc[((np.isnan(X["Fare"])) & (X.Pclass == '2') & (X.Sex == 'male')), "Fare"] = 19.0605198019802
+        X.loc[((np.isnan(X["Fare"])) & (X.Pclass == '3') & (X.Sex == 'female')), "Fare"] = 16.688050
+        X.loc[((np.isnan(X["Fare"])) & (X.Pclass == '3') & (X.Sex == 'male')), "Fare"] = 12.652100
 
         return X
-
-
-class FeatureSelector(BaseEstimator, TransformerMixin):
-    def __init__(self, feature_names):
-        self._feature_names = feature_names
-
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X, y=None):
-        return X[self._feature_names]
 
 
 class CategoricalTransformer(BaseEstimator, TransformerMixin):
@@ -59,9 +57,10 @@ class CategoricalTransformer(BaseEstimator, TransformerMixin):
         else:
             return 'Yes'
 
-    # Transformer method we wrote for this transformer
+    # Transformer method for this transformer
     def transform(self, X, y=None):
-        return X.values
+        # Categorical features to pass down the categorical pipeline
+        return X[['Sex']].values
 
 
 class NumericalTransformer(BaseEstimator, TransformerMixin):
@@ -72,33 +71,29 @@ class NumericalTransformer(BaseEstimator, TransformerMixin):
         return self
 
     def transform(self, X, y=None):
+        # Numerical features to pass down the numerical pipeline
+        X = X[['Pclass', 'Age', 'SibSp', 'Parch', 'Fare']]
         X = X.replace([np.inf, -np.inf], np.nan)
         return X.values
 
 
-# Categorical features to pass down the categorical pipeline
-categorical_features = ['Sex']
-
-# Numerical features to pass down the numerical pipeline
-numerical_features = ['Pclass', 'Age', 'SibSp', 'Parch', 'Fare']
-
 # Defining the steps in the categorical pipeline
-categorical_pipeline = Pipeline(steps=[('cat_selector', FeatureSelector(categorical_features)),
-                                       ('cat_transformer', CategoricalTransformer()),
-                                       ('one_hot_encoder', OneHotEncoder(sparse=False))])
+categorical_pipeline = Pipeline(steps=[
+    ('cat_transformer', CategoricalTransformer()),
+    ('one_hot_encoder', OneHotEncoder(sparse=False))])
 
 # Defining the steps in the numerical pipeline
-numerical_pipeline = Pipeline(steps=[('num_selector', FeatureSelector(numerical_features)),
-                                     ('num_transformer', NumericalTransformer()),
-                                     ('imputer', SimpleImputer(strategy='median')),
-                                     ('std_scaler', StandardScaler())])
+numerical_pipeline = Pipeline(steps=[
+    ('num_transformer', NumericalTransformer()),
+    ('imputer', SimpleImputer(strategy='median')),
+    ('std_scaler', StandardScaler())])
 
 # Combining numerical and categorical pipeline into one full big pipeline horizontally
 # using FeatureUnion
-full_pipeline = FeatureUnion(transformer_list=[
+union_pipeline = FeatureUnion(transformer_list=[
     ('categorical_pipeline', categorical_pipeline),
     ('numerical_pipeline', numerical_pipeline)])
 
-# Combining the custum imputer with the categorical and numerical pipeline
+# Combining the custom imputer with the categorical and numerical pipeline
 preprocess_pipeline = Pipeline(steps=[('custom_imputer', CustomImputer()),
-                                      ('full_pipeline', full_pipeline)])
+                                      ('full_pipeline', union_pipeline)])
